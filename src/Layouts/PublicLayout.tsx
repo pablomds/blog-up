b5cbf59758833584eb76dca7a7a46cfc77ad9a03
pablomds <moraisdesouzapablo@gmail.com>
2025-02-04 16:10:26 +0100

    adding scroll lenis

--- src/Layouts/PublicLayout.tsx
+++ src/Layouts/PublicLayout.tsx
@@ -1,15 +1,22 @@
+import Lenis from 'lenis'
+
 import NavBar from '../Components/Global/NavBar/NavBar'
 
 import LandingPage from '../Pages/Public/LandingPage'
 import AboutPage from '../Pages/Public/AboutPage'
 
 const PublicLayout = () => {
+
+  new Lenis({
+    autoRaf: true,
+  });
+
   return (
-    <div className="overflow-y-scroll hide-scrollbar h-screen">
+    <>
       <NavBar />
       <LandingPage />
       <AboutPage />
-    </div>
+    </>
   );
 };
 
