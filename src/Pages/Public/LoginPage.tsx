f568df39b1196afca8794105cc604154a50021f9
pablomds <moraisdesouzapablo@gmail.com>
2025-02-09 20:55:06 +0100

    adding login logic

--- src/Pages/Public/LoginPage.tsx
+++ src/Pages/Public/LoginPage.tsx
@@ -1,35 +1,41 @@
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import { AtSign, Eye, EyeOff } from 'lucide-react';
 import { Link } from 'react-router';
 
-import backgroundLogin from "../../Assets/Background/background-login.jpg"
+import { LoginSchema, FormLogin } from '../../Schemas/LoginSchema';
+import { logInWithEmailAndPassword } from '../../Firebase/authentication';
 
 import Input from '../../Components/Global/Input/Input';
 import Loader from '../../Components/Global/Loader/Loader';
 
-const loginSchema = yup.object({
-  email: yup.string().required("Your email is required").email("Your email must be valid"),
-  password: yup.string().required("Your password is required"),
-});
-
-type FormLogin = yup.InferType<typeof loginSchema>;
+import backgroundLogin from "../../Assets/Background/background-login.jpg"
+import { useToast } from '../../Context/ToastContext';
 
 const LoginPage = () => {
 
+  const toast = useToast();
   const [isFormSubmited, setIsFormSubmited] = useState(false);
   const [showPassword, setShowPassword] = useState(true);
 
     const { register, handleSubmit,control, formState: { errors } } = useForm<FormLogin>({
-      resolver: yupResolver(loginSchema),
+      resolver: yupResolver(LoginSchema),
       mode: "onSubmit"
     });
 
     const onSubmit = async (formLogin: FormLogin) => {
-      console.log(formLogin);
-      setIsFormSubmited(true);
+      const { email, password } = formLogin;
+      try {
+        const response = await logInWithEmailAndPassword(email, password);
+        if (response.status === "success") {
+          toast?.open("You're now logged in!", response.status);
+        } else {
+          toast?.open("Email/Password incorrect!", response.status)
+        }
+      } catch (error) {
+        console.log("Error On Submit()", error)
+      }
     }
 
   return (
