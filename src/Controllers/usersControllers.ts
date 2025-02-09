3f4bdc815d029433b4a41fd5ff70b01870ded99f
pablomds <moraisdesouzapablo@gmail.com>
2025-02-09 20:03:47 +0100

    creating userControllers

---
+++ src/Controllers/usersControllers.ts
@@ -0,0 +1,11 @@
+import { addDocumentToCollection } from "../Firebase/firebaseFunctions";
+import { COLLECTIONS } from "../Firebase/collections";
+
+export const createUser = async (email: string, password: string, uid: string): Promise<string | void> => {
+    try {
+        const createdUserId = await addDocumentToCollection(COLLECTIONS.USERS, { email, password, uid, auth_provider: "local" });
+        return createdUserId
+    } catch (error) {
+        console.log('Error On createUser()', error)
+    }
+}
