fb382271712c8657b6cc3f11a7af0855527278e9
pablomds <moraisdesouzapablo@gmail.com>
2025-02-10 23:36:19 +0100

    adding new routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,25 +1,37 @@
-import { Route } from 'react-router'
+import { Navigate, Route } from 'react-router';
+import { useSelector } from "react-redux";
+
+import { selectUser } from '../Redux/Slices/userSlice';
+import { useNavigate } from 'react-router';
 
 import PublicLayout from '../Layouts/PublicLayout'
 import PrivateLayout from '../Layouts/PrivateLayout'
 import LoginPage from '../Pages/Public/LoginPage'
 import SignUpPage from '../Pages/Public/SignUpPage'
-import PrivatePage from '../Pages/PrivatePage'
+import LastestPage from '../Pages/Private/LastestPage';
+import BlogPostDetailsPage from '../Pages/Private/BlogPostDetailsPage';
+import CreateNewPost from '../Pages/Private/CreateNewPostPage';
+
 
 export const PublicRoutes = () => {
-  return (
+  return [
     <Route>
-      <Route path="/" element={<PublicLayout />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route key="lading-page" path="/" element={<PublicLayout />} />
+      <Route key="login-page" path="/login" element={<LoginPage />} />
+      <Route key="signup-page" path="/sign-up" element={<SignUpPage />} />
     </Route>
-  )
-}
+  ]
+};
 
 export const PrivateRoutes = () => {
-    return (
-        <Route path="/private-routes/*" element={<PrivateLayout />}>
-          <Route  element={<PrivatePage  />} />
-        </Route>
-    );
-}
+
+  const currentUser = useSelector(selectUser);
+
+  return (
+    <Route element={<PrivateLayout currentUser={currentUser} />}>
+      <Route key="lastest-page" path="/lastest" element={<LastestPage />} />
+      <Route key="post-details-page" path="/post" element={<BlogPostDetailsPage />} />
+      <Route key="create-new-post-page" path="/create-new-post" element={<CreateNewPost />} />
+    </Route>
+  );
+};
