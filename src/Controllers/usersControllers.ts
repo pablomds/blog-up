de6e73f7045da1f491c28e35446e58aead14c9b4
pablomds <moraisdesouzapablo@gmail.com>
2025-02-10 23:35:01 +0100

    changing some used atributes

--- src/Controllers/usersControllers.ts
+++ src/Controllers/usersControllers.ts
@@ -1,9 +1,9 @@
 import { addDocumentToCollection } from "../Firebase/firebaseFunctions";
 import { COLLECTIONS } from "../Firebase/collections";
 
-export const createUser = async (email: string, password: string, uId: string): Promise<string | void> => {
+export const createUser = async (email: string, password: string, uid: string): Promise<string | void> => {
     try {
-        const createdUserId = await addDocumentToCollection(COLLECTIONS.USERS, { email, password, uId, authProvider: "local" });
+        const createdUserId = await addDocumentToCollection(COLLECTIONS.USERS, { email, password, uid, authProvider: "local" });
         return createdUserId
     } catch (error) {
         console.log('Error On createUser()', error)
