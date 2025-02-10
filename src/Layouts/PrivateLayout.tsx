507029d9e5547524b576191d58d801322c6fa74b
pablomds <moraisdesouzapablo@gmail.com>
2025-02-10 23:36:09 +0100

    adapting private layout for now

--- src/Layouts/PrivateLayout.tsx
+++ src/Layouts/PrivateLayout.tsx
@@ -1,14 +1,29 @@
-import React, { ReactNode } from 'react'
-import { Outlet } from 'react-router'
+import React, { ReactNode, useEffect } from 'react'
+import { Outlet } from 'react-router';
+import { useNavigate } from 'react-router';
+import PrivateNavBar from '../Components/Global/NavBar/PrivateNavBar';
 
 interface IPrivateLayout {
     children?: ReactNode,
-    currentUser?: any | undefined
+    currentUser?: any | null
 }
 
 const PrivateLayout: React.FC<IPrivateLayout> = (props) => {
+  // const navigate = useNavigate();
+  // navigate("/login");
+
+  // useEffect(() => {
+  //   // Redirect if no currentUser
+  //   if (!props.currentUser) {
+  //     navigate("/login");
+  //   }
+  // }, [props.currentUser, navigate]); // Run effect when currentUser changes
+
   return (
-    <div>{props.children ? props.children : <Outlet />}</div>
+    <div className="relative h-screen w-screen mx-auto px-6 pt-20 md:pl-40 md:pr-40 lg:pr-64 xl:pr-96 md:pt-12">
+      <PrivateNavBar />
+      {props.children ? props.children : <Outlet />}
+    </div>
   )
 };
 
