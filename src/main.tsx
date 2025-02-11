7c5a8225e1c2ab7a99095af1a4866d9d9c04797b
pablomds <moraisdesouzapablo@gmail.com>
2025-02-11 18:37:47 +0100

    initialize persistor

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import Lenis from 'lenis'
-import store from './Redux/store';
-import './index.css'
-import App from './App.tsx'
-
+import Lenis from 'lenis';
+import { store, persistor } from './Redux/configureStore.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App.tsx';
+import './index.css';
 
 new Lenis({
   autoRaf: true,
 });
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store}>   
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
