55ebb7a88d846fa42c3c56f8a7a3e84446dfbd2c
pablomds <moraisdesouzapablo@gmail.com>
2025-02-11 19:00:44 +0100

    adpating privatelayout in order to only render if there's a user

--- src/Layouts/PrivateLayout.tsx
+++ src/Layouts/PrivateLayout.tsx
@@ -1,6 +1,5 @@
-import React, { ReactNode, useEffect } from 'react'
-import { Outlet } from 'react-router';
-import { useNavigate } from 'react-router';
+import React, { ReactNode } from 'react'
+import { Navigate, Outlet } from 'react-router';
 import PrivateNavBar from '../Components/Global/NavBar/PrivateNavBar';
 
 interface IPrivateLayout {
@@ -9,15 +8,10 @@ interface IPrivateLayout {
 }
 
 const PrivateLayout: React.FC<IPrivateLayout> = (props) => {
-  const navigate = useNavigate();
-  navigate("/login");
 
-  useEffect(() => {
-    // Redirect if no currentUser
-    if (!props.currentUser) {
-      navigate("/login");
-    }
-  }, [props.currentUser, navigate]); // Run effect when currentUser changes
+  if (!props.currentUser) {
+    return <Navigate to="/login" replace />;
+  }
 
   return (
     <div className="relative h-screen w-screen mx-auto px-6 pt-20 md:pl-40 md:pr-40 lg:pr-64 xl:pr-96 md:pt-12">
