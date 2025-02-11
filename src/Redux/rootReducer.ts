7c90b54e0937bcb84619c8d0b808ea6716f57bf4
pablomds <moraisdesouzapablo@gmail.com>
2025-02-11 18:52:49 +0100

    setting up also adding reset action in order to reset all state if needed

--- src/Redux/rootReducer.ts
+++ src/Redux/rootReducer.ts
@@ -2,9 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import userReducer from './Slices/userSlice';
+import userReducer from "./Slices/userSlice";
+import authReducer from "./Slices/authSlice";
 // Import other reducers if needed
 // import authReducer from './Slices/authSlice';
+
+const RESET_ACTION = 'RESET_ACTION';
 
 // Persist Config
 const persistConfig = {
@@ -12,11 +15,15 @@ const persistConfig = {
   storage,
 };
 
-// Root Reducer (combine all reducers)
-const rootReducer = combineReducers({
-  user: userReducer,
-  // auth: authReducer, // Add more reducers here
-});
+const rootReducer = (state: any, action: any) => {
+  if (action.type === RESET_ACTION) {
+    state = undefined; // This will reset all slices to their initial state
+  }
+  return combineReducers({
+    user: userReducer,
+    auth: authReducer,
+  })(state, action);
+};
 
 // Persisted Root Reducer
 export const persistedReducer = persistReducer(persistConfig, rootReducer);
