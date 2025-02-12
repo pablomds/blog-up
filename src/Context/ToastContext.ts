655d550d67b4e9c026f56828ebc764e8e241ec39
pablomds <moraisdesouzapablo@gmail.com>
2025-02-12 23:26:15 +0100

    remove toast used before

--- src/Context/ToastContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-type ToastContextValue = {
-    open: (message: string, variant: "success" | "failed") => void;
-}
-
-export const ToastContext = createContext<ToastContextValue | null>(null);
-
-export const useToast = () => useContext(ToastContext)
