ed69c98b2a215d6f63fbc8163600db43b95c3281
pablomds <moraisdesouzapablo@gmail.com>
2025-02-12 17:33:41 +0100

    removing unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Routes, BrowserRouter as Router } from 'react-router';
-import { Provider } from 'react-redux';
 
-import store from './Redux/store';
 import { PublicRoutes, PrivateRoutes } from "./Routes/Routes";
 import { ToastProvider } from './Components/Global/Toast/Toast.tsx'
 import './App.css';
