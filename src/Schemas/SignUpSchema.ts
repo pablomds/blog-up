738f0ef0f56a58c7ba272044889162f62849498b
pablomds <moraisdesouzapablo@gmail.com>
2025-02-13 13:21:00 +0100

    adding validation to signup user

--- src/Schemas/SignUpSchema.ts
+++ src/Schemas/SignUpSchema.ts
@@ -1,14 +1,20 @@
 import * as yup from 'yup';
 
 export const SignUpSchema = yup.object({
-  name: yup.string().required("Your name is required"),
+  name: yup
+    .string()
+    .matches(/^[a-zA-Z\s]+$/, "Name must contain only letters and spaces")
+    .max(35, "Name must have less than 35 characters")
+    .required("Your name is required"),
   email: yup
     .string()
     .required("Your email is required")
     .email("Your email must be valid"),
   password: yup
     .string()
-    .min(8, "Your password must have 8 characters at least")
+    .matches(/[a-zA-Z]/, "Password must contain at least one letter")
+    .matches(/[^a-zA-Z0-9]/, "Password must contain at least one special character")
+    .min(8, "Password must have 8 characters at least")
     .required("Your password is required"),
   confirmPassword: yup
     .string()
