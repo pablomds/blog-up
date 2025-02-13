54d72e3e30638bbe1d7744571d56ad4f0e754e55
pablomds <moraisdesouzapablo@gmail.com>
2025-02-13 20:11:57 +0100

    lastest page imports

--- src/Pages/Private/LastestPage.tsx
+++ src/Pages/Private/LastestPage.tsx
@@ -1,14 +1,14 @@
-import { selectPosts } from '../../Redux/Slices/postsSlice';
 import _ from 'lodash';
 import { useSelector } from 'react-redux';
-import BlogPosts from '../../Components/BlogPosts/BlogPosts';
 
+import BlogPosts from '../../Components/BlogPosts/BlogPosts';
+import { selectPosts } from '../../Redux/Slices/postsSlice';
 
 const LastestPage = () => {
   const blogPosts = useSelector(selectPosts);
 
   return (
-    <div className="h-full w-full flex flex-col items-start gap-y-7">
+    <div className="h-full w-full flex flex-col items-start gap-y-7 pb-8">
       <div className="flex flex-col items-center">
         <div className="h-[5px] w-[44px] bg-blog-up-green" />
         <h1 className="font-inria-sans text-2xl">Lastest</h1>
