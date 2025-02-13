818976724927039b8206b4472d6b7534f016b8fe
pablomds <moraisdesouzapablo@gmail.com>
2025-02-13 11:51:20 +0100

    refactor of user avatar

--- src/Components/Global/NavBar/MobilePrivateNavBar.tsx
+++ src/Components/Global/NavBar/MobilePrivateNavBar.tsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router';
 import { Search, CirclePlus, TrendingUp, LogOut } from 'lucide-react';
 import { resetState } from '../../../Redux/rootReducer';
 import { useDispatch } from 'react-redux';
+import UsernameLogo from './PrivateNavBar/UsernameLogo';
 
 
-const MobilePrivateNavBar = ({ navLinks }: any) => {
+interface IDesktopPrivateSideBar {
+    navLinks: any,
+    username: string
+}
+
+const MobilePrivateNavBar: React.FC<IDesktopPrivateSideBar> = ({ navLinks, username }: any) => {
     const dispatch = useDispatch()
-    const UserAvatar = ({ username } : { username: string}) => {
-        return (
-            <div className="bg-blog-up-green h-10 w-10 rounded-full flex justify-center items-center">
-                <span className="font-inria-sans text-xl font-bold text-blog-up-black">{username.charAt(0).toUpperCase()}</span>
-            </div>
-        )
-    };
+
   return (
     <div className="md:hidden min-w-[310px] xs:w-[350px] sm:w-[400px] min-h-[74px] bg-blog-up-black fixed bottom-0 left-1/2 -translate-1/2 border-2 border-blog-up-green rounded-[5px] flex justify-between items-center px-10 z-50">
-        <Link to={navLinks[0].link}><UserAvatar username="Pablo" /></Link>
+        <UsernameLogo username={username} link={navLinks[0].link} />
         <Link to={navLinks[1].link}><Search className="w-10 h-10 text-blog-up-green" /></Link>
         <Link to={navLinks[2].link}><TrendingUp className="w-10 h-10 text-blog-up-green" /></Link>
         <Link to={navLinks[3].link}><CirclePlus className="w-10 h-10 text-blog-up-green" /></Link>
