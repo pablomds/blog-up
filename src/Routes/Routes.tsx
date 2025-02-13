d90e6e39984c0c32c21a4536eb2f5631ea24daff
pablomds <moraisdesouzapablo@gmail.com>
2025-02-13 12:45:59 +0100

    adding searchpage to routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -11,6 +11,7 @@ import LastestPage from '../Pages/Private/LastestPage';
 import BlogPostDetailsPage from '../Pages/Private/BlogPostDetailsPage';
 import CreateOrEditPostPage from '../Pages/Private/CreateOrEditPostPage';
 import MyProfilPage from '../Pages/Private/MyProfilPage';
+import SearchPostPage from '../Pages/Private/SearchPostPage';
 import NotFoundPage from '../Pages/Public/NotFoundPage';
 
 
@@ -32,6 +33,7 @@ export const PrivateRoutes = () => {
   return (
     <Route element={<PrivateLayout currentUser={currentUser} />}>
       <Route key="lastest-page" path="/my-profil" element={<MyProfilPage />} />
+      <Route key="lastest-page" path="/search" element={<SearchPostPage />} />
       <Route key="lastest-page" path="/lastest" element={<LastestPage />} />
       <Route key="post-details-page" path="/post/:id" element={<BlogPostDetailsPage />} />
       <Route key="create-new-post-page" path="/create-post/:id?" element={<CreateOrEditPostPage />} />
