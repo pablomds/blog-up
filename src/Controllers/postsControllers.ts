afdbce48fee4ca122332fbaa5bdbf2b105918324
pablomds <moraisdesouzapablo@gmail.com>
2025-02-15 13:34:50 +0100

    new methods to fetch posts

--- src/Controllers/postsControllers.ts
+++ src/Controllers/postsControllers.ts
@@ -1,4 +1,4 @@
-import { addDocumentToCollection, deleteDocumentFromCollection, getAllDataFromCollection, getDataFromCollection, updateDocumentToCollection } from "../Firebase/firebaseFunctions";
+import { addDocumentToCollection, deleteDocumentFromCollection,getAllDataFromCollectionWithIds, getAllDataFromCollection, getDataFromCollection, getPaginatedDataFromCollection, getTotalDataInCollection, updateDocumentToCollection } from "@/Firebase/firebaseFunctions";
 
 import { COLLECTIONS } from "@/Firebase/collections";
 
@@ -34,7 +34,25 @@ export const updatePost = async (postId: string, dataToUpdate: any) => {
     } catch (error) {
         console.log("Error On updatePost()", error)
     }
-}
+};
+
+export const getPaginatedPosts = async (limitNumber: number, orderedBy: string = "createdDate", order: "asc" | "desc" = "desc", lastDoc = null) => {
+    try {
+        const paginatedPosts = await getPaginatedDataFromCollection(COLLECTIONS.POSTS,limitNumber,orderedBy,order,lastDoc);
+        return paginatedPosts;
+    } catch (error) {
+        console.log("Error On getLastestPosts", error);
+    }
+};
+
+export const getTotalPosts = async () => {
+    try {
+        const totalPosts = await getTotalDataInCollection(COLLECTIONS.POSTS);
+        return totalPosts;
+    } catch (error) {
+        console.log("Error On getTotalPosts()", error);
+    }
+};
 
 export const getPosts = async () => {
     try {
@@ -44,3 +62,12 @@ export const getPosts = async () => {
         console.log("Error On getPosts()", error)
     }
 };
+
+export const getPostsWithIds = async (ids: string[]) => { 
+    try {
+        const posts = await getAllDataFromCollectionWithIds(COLLECTIONS.POSTS, ids);
+        return posts
+    } catch (error) {
+        console.log("Error On getPostsWithIds()", error)
+    }
+}
