08ddac1f03fefebd01ceda6081dbc034e2d81247
pablomds <moraisdesouzapablo@gmail.com>
2025-02-16 17:37:32 +0100

    conditionnaly displaying not found posts

--- src/Components/BlogPosts/BlogPosts.tsx
+++ src/Components/BlogPosts/BlogPosts.tsx
@@ -3,6 +3,10 @@ import _ from 'lodash';
 
 import BlogPost from '@/Components/BlogPosts/BlogPost';
 import Pagination from '@/Components/Pagination/Pagination';
+import NoPostsFound from './NoPostsFound';
+
+
+
 
 interface IBlogPosts {
     blogPosts: any;
@@ -12,12 +16,22 @@ interface IBlogPosts {
     onPageChange:(page: number) => void;
 };
 
-const BlogPosts: React.FC<IBlogPosts> = ({ blogPosts, currentPage, totalPosts,itemsPerPage, onPageChange }) => {
+const BlogPosts: React.FC<IBlogPosts> = ({
+  blogPosts,
+  currentPage,
+  totalPosts,
+  itemsPerPage,
+  onPageChange,
+}) => {
   const getPostsByCreatedDateOrder = () =>
     _.chain(blogPosts)
       .orderBy("createdDate", "desc")
       .map((post, index) => <BlogPost post={post} key={index} />)
       .value();
+
+  if (blogPosts.length === 0) {
+    return <NoPostsFound />;
+  }
   return (
     <div className="flex flex-col gap-y-7">
       <Pagination
