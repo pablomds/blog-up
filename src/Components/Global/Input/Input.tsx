b13f5ae04f2ff4f567c66d7924ec6e797e3fe7f1
pablomds <moraisdesouzapablo@gmail.com>
2025-02-17 23:43:14 +0100

    working on input adding disabled

--- src/Components/Global/Input/Input.tsx
+++ src/Components/Global/Input/Input.tsx
@@ -6,21 +6,24 @@ interface IInputProps extends HTMLProps<HTMLInputElement>{
   type?: string | "text";
   error?: string;
   icon?: ReactNode;
+  isDisabled?: boolean
 };
 
 const Input = forwardRef((props: IInputProps, ref: Ref<HTMLInputElement>) =>  {
-  const { placeholder, error, icon , ...IInputProps} =  props;
+  const { placeholder, error, icon, isDisabled, ...IInputProps} =  props;
   return (
     <div className="flex flex-col relative w-full max-w-[508px]">
       <div className="relative w-full">
         <input
-          className={`font-inria-sans w-full h-14 border-2 border-blog-up-green py-4 pl-6 pr-12 rounded-[5px] focus:outline-none`}
+          disabled={isDisabled}
+          readOnly={isDisabled}
+          className={`font-inria-sans w-full h-14 border-2 py-4 pl-6 pr-12 rounded-[5px] focus:outline-none ${isDisabled ? 'border-blog-up-gray/70 text-blog-up-gray/70' :'border-blog-up-green'}`}
           ref={ref}
           {...IInputProps}
           placeholder={placeholder}
         />
         {icon && (
-          <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-blog-up-green">
+          <span className={`absolute right-4 top-1/2 transform -translate-y-1/2 ${isDisabled ? 'text-blog-up-gray/70' :'text-blog-up-green'}`}>
             {icon}
           </span>
         )}
