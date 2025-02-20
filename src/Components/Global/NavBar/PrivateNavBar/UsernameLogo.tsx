f9af21c027e69d91e540ddecc728ce06abbf5b07
pablomds <moraisdesouzapablo@gmail.com>
2025-02-20 16:40:04 +0100

    text bigger for navbar

--- src/Components/Global/NavBar/PrivateNavBar/UsernameLogo.tsx
+++ src/Components/Global/NavBar/PrivateNavBar/UsernameLogo.tsx
@@ -9,7 +9,7 @@ interface IUsernameLogo {
 const UsernameLogo: React.FC<IUsernameLogo> = ({ username, link }) => {
   return (
     <Link to={link} className="bg-blog-up-green w-10 h-10 xl:h-14 xl:w-14 hover:bg-blog-up-gray rounded-full flex justify-center items-center select-none">
-      <span className="font-inria-sans text-base xl:text-4xl font-bold text-blog-up-black">
+      <span className="font-inria-sans text-xl xl:text-4xl font-bold text-blog-up-black">
         {username.charAt(0).toUpperCase()}
       </span>
     </Link>
