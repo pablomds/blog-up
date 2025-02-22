8c06f81425de3b339ad8dc61db96505552f26166
pablomds <moraisdesouzapablo@gmail.com>
2025-02-22 14:48:26 +0100

    min character for posts

--- src/Schemas/PostSchema.ts
+++ src/Schemas/PostSchema.ts
@@ -2,9 +2,10 @@ import * as yup from 'yup';
 
 export const PostSchema = yup.object({
   id: yup.string().notRequired().default(""),
-  title: yup.string().max(60, "Your title must have less than 60 characters").required("Your title is required"),
+  title: yup.string().min(5, "Your title must have more at least 5 characters").max(60, "Your title must have less than 60 characters").required("Your title is required"),
   text: yup
     .string()
+    .min(200, "Your test must have at least 200 characters")
     .max(1000, "Your text must have less than 1000 characters")
     .required("Your text is required")
 });
